cc056a353a47498708d1694d63ffcbeaa1558f83
Nicolás Salas <nsalas@fastmail.com>
2022-07-10 05:41:27 -0400

    add catppuccin theme and change font

--- config.def.h
+++ config.def.h
@@ -4,13 +4,13 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Iosevka NerdFont Mono:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#d0e0ee", "#1e1e2e" },
+	[SchemeSel] = { "#1e1e2e", "#96cdfb" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
